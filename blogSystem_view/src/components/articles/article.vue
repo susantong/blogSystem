f9c57a8d72450f91c8e33852af9dca2091179ef7
susantong <1316160497@qq.com>
2017-04-28 23:04:49 +0800

    修改文章展示

--- blogSystem_view/src/components/articles/article.vue
+++ blogSystem_view/src/components/articles/article.vue
@@ -1,14 +1,19 @@
 <template>
-	<div class="article">
+	<div class="articles">
 		<h2>{{data.list.title}}</h2>
-		<p>{{data.list.contents}}</p>
+		<p class="artcle-tit">
+			<span>发布时间: {{data.list.time}}</span>
+			<span>编辑: {{data.list.editor}}</span>
+			<span>阅读量: {{data.list.watchNum}}</span>
+		</p>
+		<p class="artcle-text">{{data.list.contents}}</p>
 	</div>
 </template>
 
 <script>
 import getCGI from '../../getCGI/article'
 let data = {
-	list: []
+	list: {}
 };
 getCGI(data);
 
@@ -22,4 +27,29 @@ getCGI(data);
 	}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+	.articles {
+		h2 {
+			font-size: 24px;
+			font-weight: bold;
+			text-align: center;
+			margin-bottom: 20px;
+			color: #999;
+		}
+		.artcle-tit {
+			border: 1px dotted #ccc;
+			text-align: center;
+			span:first-child, span:nth-child(2) {
+				margin-right: 20px;
+			}
+			height: 24px;
+			line-height: 24px;
+		}
+		.artcle-text {
+			margin-top: 20px;
+			text-indent: 2em;
+			font-size: 16px;
+			line-height: 24px;
+		}
+	}
+</style>
